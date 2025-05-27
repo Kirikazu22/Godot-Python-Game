d919c4d84982681b1f22499f0efee6330827caa8
Douglas Gabriel da Costa Alves <104402297+Kirikazu22@users.noreply.github.com>
2025-05-27 09:32:33 -0300

    Add files via upload

--- godot-python-game/python/dist/index.service.worker.js
+++ godot-python-game/python/dist/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1746632699|6790879874";
+const CACHE_VERSION = "1748005383|1095907492";
 const CACHE_PREFIX = "projeto_jogo_pyt-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
